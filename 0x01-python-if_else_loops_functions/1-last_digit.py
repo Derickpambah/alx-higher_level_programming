51255b8ca7e1934956e2201a565e6b11368d32d5
Derickpambah <pambahderick412@gmail.com>
2022-01-04 18:53:46 -0800

    Code to solve for the last digit in a number
    solved last digit value problem

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+import random
+number = random.randint(-10000, 10000)
+last_number  = number % 10
+
+def last_num(number):
+    if last_number == 0:
+        return "Last digit of", number, "is", last_number, "and is 0"
+    elif last_number > 5:
+        return "Last digit of", number, "is", last_number, "and is greater than 5"
+    else:
+        return "Last digit of", number, "is", last_number, "and is less than 6 and not 0"
